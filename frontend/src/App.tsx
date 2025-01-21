8a5485b325c414932eb69074ba36e277916d6268
unknown <87129505+lilkajt@users.noreply.github.com>
2025-01-21 23:10:27 +0100

    created pages and routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import About from "./pages/About"
+import SignIn from "./pages/SignIn"
+import Profile from "./pages/Profile"
+import Home from "./pages/Home"
+import SignUp from "./pages/SignUp"
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-center">
-      Hello world!
-      </h1>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
