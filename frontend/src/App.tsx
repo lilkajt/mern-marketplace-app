612105223afe7207d0d1be0c6fa2a6f8a2457104
unknown <87129505+lilkajt@users.noreply.github.com>
2025-01-22 22:12:22 +0100

    create header component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,17 +4,19 @@ import SignIn from "./pages/SignIn"
 import Profile from "./pages/Profile"
 import Home from "./pages/Home"
 import SignUp from "./pages/SignUp"
+import Header from "./components/Header"
 function App() {
   return (
     <>
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
       </BrowserRouter>
     </>
   )
