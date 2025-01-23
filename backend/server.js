efe6fa6d3604460432ec5f7de876223ca5c1c8fc
unknown <87129505+lilkajt@users.noreply.github.com>
2025-01-23 21:27:32 +0100

    create user model

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { connectDB } from './config/db.js';
+import { connectDB } from './config/Db.js';
 
 const app = express();
 app.use(cors());
