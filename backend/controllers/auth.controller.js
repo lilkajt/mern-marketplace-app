5240998e060ec59f5463ab066d7886c7c3c3fbd5
unknown <87129505+lilkajt@users.noreply.github.com>
2025-01-23 22:31:15 +0100

    create sign up api route

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+import bcrypt from "bcryptjs";
+import mongoose from "mongoose";
+import User from "../models/user.model.js";
+
+export const signup = async (req, res) => {
+    const {username, email, password } = req.body;
+    const hashedPassword = bcrypt.hashSync(password, 12);
+    const newUser = new User({ username, email, password: hashedPassword });
+    try {
+        await newUser.save();
+        res.status(201).json({ message: "User created successfully!" });
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong" });
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import { connectDB } from './config/Db.js';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 app.listen(3001, () => {
     connectDB();
     console.log('Server running on http://localhost:3001');
