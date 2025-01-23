a56f677b4d6b9830b35d966071b1bb81a7f9c894
unknown <87129505+lilkajt@users.noreply.github.com>
2025-01-23 21:18:26 +0100

    connecte to db

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import { connectDB } from './config/db.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.listen(3001, () => {
-  console.log('Server running on port 3001');
+    connectDB();
+    console.log('Server running on port 3001');
 });
