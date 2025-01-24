04318148fa5da81a2febc2b530512acd098810ef
unknown <87129505+lilkajt@users.noreply.github.com>
2025-01-24 20:01:57 +0100

    create middleware + func to handle possible errors

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import bcrypt from "bcryptjs";
 import mongoose from "mongoose";
 import User from "../models/user.model.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const {username, email, password } = req.body;
     const hashedPassword = bcrypt.hashSync(password, 12);
     const newUser = new User({ username, email, password: hashedPassword });
@@ -10,6 +10,6 @@ export const signup = async (req, res) => {
         await newUser.save();
         res.status(201).json({ message: "User created successfully!" });
     } catch (error) {
-        res.status(500).json({ message: "Something went wrong" });
+        next(error);
     }
 }
--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,15 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    });
+});
 app.listen(3001, () => {
     connectDB();
     console.log('Server running on http://localhost:3001');
